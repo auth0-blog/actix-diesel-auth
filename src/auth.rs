28cf6c002fa322fa7d3b06b38698ad9ced86f34b
Abhishek Chanda <abhishek.chanda@auth0.com>
2020-04-21 12:02:27 +0100

    Rename domain to authority

--- src/auth.rs
+++ src/auth.rs
@@ -11,10 +11,10 @@ struct Claims {
 }
 
 pub fn validate_token(token: &str) -> Result<bool, ServiceError> {
-    let domain = std::env::var("DOMAIN").expect("DOMAIN must be set");
-    let jwks = fetch_jwks(&format!("{}{}", domain.as_str(), ".well-known/jwks.json"))
+    let authority = std::env::var("AUTHORITY").expect("AUTHORITY must be set");
+    let jwks = fetch_jwks(&format!("{}{}", authority.as_str(), ".well-known/jwks.json"))
         .expect("failed to fetch jwks");
-    let validations = vec![Validation::Issuer(domain), Validation::SubjectPresent];
+    let validations = vec![Validation::Issuer(authority), Validation::SubjectPresent];
     let kid = match token_kid(&token) {
         Ok(res) => res.expect("failed to decode kid"),
         Err(_) => return Err(ServiceError::JWKSFetchError),
